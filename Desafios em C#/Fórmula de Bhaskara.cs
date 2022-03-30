0813652910a689754080b2bbc02d2f00de925fa6
Thais Raqueli Rezende <99492603+Thaisraqueli@users.noreply.github.com>
2022-03-30 19:34:00 -0300

    Update Fórmula de Bhaskara.cs

--- Desafios em C#/Fórmula de Bhaskara.cs
+++ Desafios em C#/Fórmula de Bhaskara.cs
@@ -1,4 +1,6 @@
-/*Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes, 
+/*Desafio
+
+Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes, 
 mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.
 
 Entrada
@@ -7,8 +9,8 @@ Leia três valores de ponto flutuante (double) A, B e C.
 Saída
 Se não houver possibilidade de calcular as raízes,
 apresente a mensagem "Impossivel calcular". Caso contrário, imprima o resultado das raízes com 5 dígitos após o
-ponto, com uma mensagem correspondente conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.
-*/
+ponto, com uma mensagem correspondente conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.*/
+
 
 using System;
 using System.Collections.Generic;
