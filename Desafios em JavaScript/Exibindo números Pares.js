b9fee29be61f603f7afb87b2a5811a03068119b4
Thais Raqueli Rezende <99492603+Thaisraqueli@users.noreply.github.com>
2022-03-30 19:54:44 -0300

    Create Exibindo números Pares.js

---
+++ Desafios em JavaScript/Exibindo números Pares.js
@@ -0,0 +1,25 @@
+/*Desafios
+
+Crie um programa que leia um número e mostre os números pares até esse número, inclusive ele mesmo.
+
+-Entrada
+Você receberá 1 valor inteiro N, onde N > 0.
+
+-Saída
+Exiba todos os números pares até o valor de entrada, sendo um em cada linha. 
+
+ 
+Exemplo de Entrada: 6;
+Exemplo de Saída: 2,4,6;
+*/
+
+let numero = gets();
+
+let par = 2;
+
+while (par <= numero) {
+  
+  console.log(par);
+  par += 2;
+
+}
